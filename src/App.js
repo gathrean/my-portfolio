586c64ba492dd6d71bfcb6c5728b2d13c75671d1
Gathrean Dela Cruz <gathrean@icloud.com>
2024-01-16 19:28:14 -0800

    Removed old sidebar code in App.js

--- src/App.js
+++ src/App.js
@@ -21,90 +21,25 @@ import DungeonQuad from './Components/Projects/DungeonQuad';
 import Nebula from './Components/Projects/Nebula';
 import OrcaSwipe from './Components/Projects/OrcaSwipe'
 
-const sectionRefs = {};
-
 function App() {
-  const [activePage, setActivePage] = useState('Home');
-
-  // Define section data
-  const sections = [
-    { id: 'Home', component: Home },
-    { id: 'Nebula', component: Nebula },
-    { id: 'OrcaSwipe', component: OrcaSwipe },
-    { id: 'DungeonQuad', component: DungeonQuad },
-    { id: 'Contact', component: Contact },
-  ];
-
-  // Handle click on a page link in the sidebar
-  const handlePageClick = (page) => {
-    setActivePage(page);
-    scrollToSection(page);
-  };
-
-  // Scroll to a section by its id
-  const scrollToSection = (section) => {
-    const ref = sectionRefs[section] || sectionRefs.about;
-    ref.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'nearest',
-    });
-  };
-
-  // Set the active page based on the section that is in view
-  useEffect(() => {
-    const observerOptions = {
-      rootMargin: '-100px 0px -100px 0px',
-    };
-
-    // Handle intersection of a section with the viewport
-    const handleIntersection = (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setActivePage(entry.target.id);
-        }
-      });
-    };
-
-    // Create an intersection observer instance
-    const observer = new IntersectionObserver(handleIntersection, observerOptions);
-
-    // Observe the section refs
-    sections.forEach((section) => {
-      const sectionElement = sectionRefs[section.id]; // Get the actual DOM element
-      if (sectionElement) {
-        observer.observe(sectionElement);
-      }
-    });
-
-    return () => {
-      observer.disconnect();
-    };
-  }, [sections]); // Add sections as a dependency to ensure observer gets updated when sections change
-
   return (
     <div className="container">
-      {/* Banner Section */}
+
       <div className="banner-section">
         <img src={bannerImage} alt="Banner" />
+
         <div className="banner-text">
-          <h1 className="banner-title">Gathrean Dela Cruz</h1>
+
+          <h1 className="banner-title benzin-regular">Gathrean Dela Cruz</h1>
+
           <p className="banner-subtitle benzin-regular">
             I AM A DEVELOPER, MUSIC PRODUCER, AND GRAPHIC DESIGNER BASED IN VANCOUVER, BC.
           </p>
+
         </div>
+
       </div>
 
-      {/* Sidebar and Content */}
-      <Sidebar activePage={activePage} handlePageClick={handlePageClick} />
-
-      <div className="content">
-        {sections.map((section) => (
-          <div className="section" id={section.id} key={section.id} ref={(el) => (sectionRefs[section.id] = el)}>
-            {section.component}
-          </div>
-        ))}
-      </div>
     </div>
   );
 }
