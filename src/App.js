222d96e46f556642c7c6da919a63c4b15aef7223
Gathrean Dela Cruz <gathrean@icloud.com>
2024-01-20 00:12:10 -0800

    Moved Banner into its own JS file and made Left Dock smaller

--- src/App.js
+++ src/App.js
@@ -10,60 +10,33 @@ import './styles/Global.css';
 import './styles/LeftDock.css';
 import './styles/Sidebar.css';
 
-import bannerImage from './assets/images/mountain.jpg';
+// import bannerImage from './assets/images/mountain.jpg';
 
 // Navigation
 // import Sidebar from './Sidebar';
 
 // Pages
+import Banner from './components/pages/Banner';
 // import Home from './components/pages/Home';
 // import Contact from './components/pages/Contact';
-
-// Projects Pages
-// import DungeonQuad from './components/projects/DungeonQuad';
-// import Nebula from './components/projects/Nebula';
-// import OrcaSwipe from './components/projects/OrcaSwipe'
 
 function App() {
 
   const [isLeftDockActive, setLeftDockActive] = useState(false);
 
   useEffect(() => {
-    // After the component mounts, trigger the animation
     setTimeout(() => {
       setLeftDockActive(true);
     }, 100);
-  }, []); // Empty dependency array to run the effect only once on mount
+  }, []);
 
   return (
     <div className="container">
-
-      <div className="banner-section">
-        {/* <img src={bannerImage} alt="Banner" /> */}
-
-        <div className="banner-text">
-
-          <h1 className="banner-title sf-regular">Gathrean <br></br>Dela Cruz</h1>
-
-          <p className="banner-subtitle montserrat-regular">
-            👋🏽 I AM A DEVELOPER & GRAPHIC DESIGNER BASED IN VANCOUVER, BC.
-          </p>
-          <br></br>
-          <p className="banner-subtitle montserrat-regular">
-            *🫡 I appreciate your curiosity, but
-            <br></br>
-            this website is currently under construction!
-          </p>
-
-        </div>
-
-      </div>
+      <Banner />
 
       <div className={`LeftDock ${isLeftDockActive ? 'active' : ''}`}>
         <div className="LeftDockItem"></div>
-        {/* Add more items as needed */}
       </div>
-
     </div>
   );
 }
