0f9f2510a35ae258cbb30cdb0a66b5c181cda890
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-17 14:30:14 -0700

    Cleared contents for homepage. Starting over

--- src/App.js
+++ src/App.js
@@ -3,86 +3,16 @@ import React, { useState, useEffect } from 'react';
 // APP //
 import './App.css';
 
-// BANNER //
-import Banner from './components/banner/Banner';
-import './components/banner/Banner.css';
-
-// SIDEBAR //
-import Sidebar from './components/sidebar/Sidebar';
-import './components/sidebar/Sidebar.css';
-
 // FONTS //
 import './assets/fonts/Fonts.css';
 
-// Project 1
-const Project1 = () => (
-  <>
-    <h2>Project Title 1</h2>
-    <p>Project Description 1</p>
-    <a href="#project-link-1">Project Link 1</a>
-    <p>Tech Stack: Project Tech Stack 1</p>
-    <p>What I Learned: What I Learned 1</p>
-  </>
-);
-
-// Project 2
-const Project2 = () => (
-  <>
-    <h2>Project Title 2</h2>
-    <p>Project Description 2</p>
-    <a href="#project-link-2">Project Link 2</a>
-    <p>Tech Stack: Project Tech Stack 2</p>
-    <p>What I Learned: What I Learned 2</p>
-  </>
-);
-
-// Project 3
-const Project3 = () => (
-  <>
-    <h2>Project Title 3</h2>
-    <p>Project Description 3</p>
-    <a href="#project-link-3">Project Link 3</a>
-    <p>Tech Stack: Project Tech Stack 3</p>
-    <p>What I Learned: What I Learned 3</p>
-  </>
-);
-
-
 function App() {
 
-  const [isSidebarActive, setSidebarActive] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setSidebarActive(true);
-    }, 100);
-  }, []);
-
   return (
-    <div className="container">
-      <Banner />
-
-      <div className={`Sidebar ${isSidebarActive ? 'active' : ''}`}>
-        <div className="SidebarItem"></div>
-        <div className="SidebarItem"></div>
-        <div className="SidebarItem"></div>
-        <div className="SidebarItem"></div>
-        <div className="SidebarItem"></div>
-        <div className="SidebarItem"></div>
+    <div className="App">
+      <div className="content">
+        <h1>Projects</h1>
       </div>
-
-      {/* Project Sections */}
-      <section id="project-link-1">
-        <Project1 />
-      </section>
-
-      <section id="project-link-2">
-        <Project2 />
-      </section>
-
-      <section id="project-link-3">
-        <Project3 />
-      </section>
     </div>
   );
 }
