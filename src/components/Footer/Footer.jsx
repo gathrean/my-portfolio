5c14cc19de7368b10ce17551dd98f1752985a3bf
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-17 22:07:27 -0700

    Update Footer component styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,11 @@ import './Footer.css';
 function Footer() {
     return (
         <footer>
-            <div className="content">
-                <p>&copy; 2024 Gathrean Dela Cruz. All rights reserved.</p>
+            <div className="content footer-body">
+                <p>&copy; 2024 Gathrean Dela Cruz</p>
+                <div className="footer-title">
+                    <h1>Gathrean <br></br>Dela Cruz</h1>
+                </div>
             </div>
         </footer>
     );
