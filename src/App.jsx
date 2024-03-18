afb767183ce2d32c2b8961576fd96c990ba7263a
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-17 22:12:49 -0700

    Refactor App component to use separate pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,32 @@
 import React from 'react';
+
+// Components //
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+
+// Pages // 
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/Contact/Contact';
 
 // CSS //
 import './App.css';
 
-// FONTS //
+// Fonts //
 import './assets/fonts/Fonts.css';
 
 function App() {
-
   return (
     <div className="App">
       <header>
         <Navbar />
       </header>
 
-      <section id="section1 home">
-        <div className="content">
-          <h2>Section 1 - Home</h2>
-
-        </div>
-      </section>
-
-      <section id="section2 about">
-        <div className="content">
-          <h2>Section 2 - About Me</h2>
-
-        </div>
-      </section>
-
-      <section id="section3 projects">
-        <div className="content">
-          <h2>Section 3 - Projects</h2>
-
-        </div>
-      </section>
-
-      <section id="section4 contact">
-        <div className="content">
-          <h2>Section 4 - Contact Me</h2>
-
-        </div>
-      </section>
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
 
       <Footer />
     </div>
