7c52fee6546cc15f3b408595c03166cef25b7396
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-17 23:12:59 -0700

    Added "dock" styling to navbar when scrolling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
 import './Navbar.css';
 
 export function Navbar() {
+    const [scroll, setScroll] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setScroll(true);
+            } else {
+                setScroll(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <nav className="navbar">
+        <nav className={`navbar ${scroll ? 'scrolled' : ''}`}>
             <div className="brand logo-home">
                 <span className="brand-text">Gathrean Dela Cruz</span>
             </div>
