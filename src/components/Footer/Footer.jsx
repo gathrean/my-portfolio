47a1ae2e8f16fa34fd6b48bcc272164e00d01abb
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-17 22:00:20 -0700

    Add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <div className="content">
+                <p>&copy; 2024 Gathrean Dela Cruz. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
