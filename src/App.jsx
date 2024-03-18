5a9e56ff9f6822658000800ac643ed1eab081a21
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-17 22:41:48 -0700

    Refactor routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Components //
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
 // Pages // 
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Projects from './pages/Projects/Projects';
-import Contact from './pages/Contact/Contact';
+import { Home } from './pages/Home/Home';
+import { About } from './pages/About/About';
+import { Projects } from './pages/Projects/Projects';
+import { Contact } from './pages/Contact/Contact';
 
 // CSS //
 import './App.css';
@@ -18,18 +19,20 @@ import './assets/fonts/Fonts.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
+    <Router>
+      <div className="App">
         <Navbar />
-      </header>
 
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
