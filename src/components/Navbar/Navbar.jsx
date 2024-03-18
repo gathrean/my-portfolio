9f447c2c267b86c5293eabaadfe2f283670acb2a
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-17 23:30:56 -0700

    Add Navbar styles for logo and mobile view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+
 import './Navbar.css';
+import './Navbar-Mobile.css'
+import './Navbar-Logo.css'
 
 export function Navbar() {
     const [scroll, setScroll] = useState(false);
