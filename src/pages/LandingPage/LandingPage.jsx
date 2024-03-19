21816cec483d7e19015482a3214cee354940bc51
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-18 22:05:14 -0700

    Add landing page carousel and images

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 // CSS //
@@ -9,32 +9,24 @@ import '../../App.css';
 import '../../assets/fonts/Fonts.css';
 
 export function Home() {
+    const [currentImage, setCurrentImage] = useState(0);
+    const images = [
+        '/images/DSC04733.jpg'
+    ];
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+        }, 5000); // Change image every 5 seconds (5000 milliseconds)
+
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <section id="section1 home">
-            <div className="content">
+            <div className="carousel-content">
+                <img src={process.env.PUBLIC_URL + images[currentImage]} alt="Landing Page" className="landing-image" />
                 <h2>Section 1 - Home</h2>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                
             </div>
         </section>
     );
