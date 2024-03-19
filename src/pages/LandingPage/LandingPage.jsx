377054d6ea17902ebd31b566d31d5564144e5243
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-19 00:30:34 -0700

    Update LandingPage.css and LandingPage.jsx

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 // CSS //
@@ -14,6 +14,9 @@ export function Home() {
         '/images/DSC04733.jpg'
     ];
 
+    // Ref for overlay text element
+    const overlayRef = useRef(null);
+
     useEffect(() => {
         const intervalId = setInterval(() => {
             setCurrentImage((prevImage) => (prevImage + 1) % images.length);
@@ -22,18 +25,35 @@ export function Home() {
         return () => clearInterval(intervalId);
     }, []);
 
+    // Function to calculate overlay text height
+    const calculateOverlayHeight = () => {
+        if (overlayRef.current) {
+            return overlayRef.current.getBoundingClientRect().height;
+        }
+        return 0;
+    };
+
     return (
         <section id="section1 home">
             <div className="carousel-content">
                 <div className="image-container">
                     <img src={process.env.PUBLIC_URL + images[currentImage]} alt="Landing Page" className="landing-image" />
-                    <div className="overlay-text">
+                </div>
+                <div className="overlay-container" style={{ marginTop: calculateOverlayHeight() }}>
+                    <div ref={overlayRef} className="overlay-text">
                         <h2>Yap yap yap</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus urna et pharetra pharetra massa. Nibh tellus molestie nunc non blandit massa. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Diam maecenas ultricies mi eget. Dis parturient montes nascetur ridiculus mus mauris. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. Vel quam elementum pulvinar etiam non quam. Purus ut faucibus pulvinar elementum. Pharetra pharetra massa massa ultricies mi quis hendrerit. At in tellus integer feugiat. Blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada. Tortor aliquam nulla facilisi cras. Lorem ipsum dolor sit amet consectetur.
-
-                            A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Posuere ac ut consequat semper. Ac orci phasellus egestas tellus rutrum. Dignissim convallis aenean et tortor at risus viverra adipiscing. Sagittis vitae et leo duis ut. Leo duis ut diam quam nulla porttitor massa. Integer eget aliquet nibh praesent tristique magna sit amet purus. Gravida in fermentum et sollicitudin ac orci phasellus. Diam phasellus vestibulum lorem sed. Vulputate ut pharetra sit amet aliquam id. Sed sed risus pretium quam vulputate dignissim. Ipsum dolor sit amet consectetur adipiscing elit. Nisl pretium fusce id velit ut tortor pretium. Et magnis dis parturient montes nascetur ridiculus mus mauris vitae. Morbi non arcu risus quis. Lobortis feugiat vivamus at augue eget. Sed odio morbi quis commodo odio aenean sed.
-                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                        <p>Tempus urna et pharetra pharetra massa. </p>
+                        <p>Nibh tellus molestie nunc non blandit massa. </p>
+                        <p>Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras.</p>
+                        <p>Diam maecenas ultricies mi eget. Dis parturient montes nascetur ridiculus mus mauris. </p>
+                        <p>Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. </p>
+                        <p>Vel quam elementum pulvinar etiam non quam. Purus ut faucibus pulvinar elementum. </p>
+                        <p>Pharetra pharetra massa massa ultricies mi quis hendrerit. At in tellus integer feugiat.</p>
+                        <p>Blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada. </p>
+                        <p>Tortor aliquam nulla facilisi cras. Lorem ipsum dolor sit amet consectetur. </p>
+                        <p>A condimentum vitae sapien pellentesque habitant morbi tristique senectus. </p>
+                        <p>Posuere ac ut consequat semper. Ac orci phasellus egestas tellus rutrum. </p>
                     </div>
                 </div>
             </div>
