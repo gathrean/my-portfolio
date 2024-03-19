f40a51e4bd768690d0f4539b72a4910b4c2bb72d
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-19 00:59:26 -0700

    Acquired sticky-scroll, but footer is still bugged out (commented out for now)

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
 
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Router>
   );
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -13,46 +13,49 @@ export function Home() {
     const images = [
         '/images/DSC04733.jpg'
     ];
-
-    // Ref for overlay text element
-    const overlayRef = useRef(null);
+    const textRef = useRef(null);
+    const [imageSticky, setImageSticky] = useState(true);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
             setCurrentImage((prevImage) => (prevImage + 1) % images.length);
         }, 5000); // Change image every 5 seconds (5000 milliseconds)
 
-        return () => clearInterval(intervalId);
+        return () => {
+            clearInterval(intervalId);
+        };
     }, []);
-
-    // Function to calculate overlay text height
-    const calculateOverlayHeight = () => {
-        if (overlayRef.current) {
-            return overlayRef.current.getBoundingClientRect().height;
-        }
-        return 0;
-    };
 
     return (
         <section id="section1 home">
             <div className="carousel-content">
                 <div className="image-container">
-                    <img src={process.env.PUBLIC_URL + images[currentImage]} alt="Landing Page" className="landing-image" />
-                </div>
-                <div className="overlay-container" style={{ marginTop: calculateOverlayHeight() }}>
-                    <div ref={overlayRef} className="overlay-text">
-                        <h2>Yap yap yap</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                    <img src={process.env.PUBLIC_URL + images[currentImage]} alt="Landing Page" className="landing-image sticky" />
+                    <div ref={textRef} className="overlay-text">
+                        <h2>Hey,  <br /> I'm Gathrean Dela Cruz</h2>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br /> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
                         <p>Tempus urna et pharetra pharetra massa. </p>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
                         <p>Nibh tellus molestie nunc non blandit massa. </p>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
                         <p>Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras.</p>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
                         <p>Diam maecenas ultricies mi eget. Dis parturient montes nascetur ridiculus mus mauris. </p>
-                        <p>Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. </p>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+                        <p>Yap yap yap, yap yap yap yap yap. Yap yap yap.</p>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
                         <p>Vel quam elementum pulvinar etiam non quam. Purus ut faucibus pulvinar elementum. </p>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
                         <p>Pharetra pharetra massa massa ultricies mi quis hendrerit. At in tellus integer feugiat.</p>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
                         <p>Blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada. </p>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
                         <p>Tortor aliquam nulla facilisi cras. Lorem ipsum dolor sit amet consectetur. </p>
+                        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
                         <p>A condimentum vitae sapien pellentesque habitant morbi tristique senectus. </p>
+                        <br /><br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
                         <p>Posuere ac ut consequat semper. Ac orci phasellus egestas tellus rutrum. </p>
                     </div>
                 </div>
