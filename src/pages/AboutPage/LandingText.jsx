4cfad1f522f00fcbe3d8f3db007539084ce58c09
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-26 13:35:19 -0700

    Fixed LandingText

---
+++ src/pages/AboutPage/LandingText.jsx
@@ -0,0 +1,31 @@
+export function LandingText() {
+  return (
+    <div>
+      <h2>Hey, <br /> I'm Gathrean Dela Cruz</h2>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br /> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Tempus urna et pharetra pharetra massa. </p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Nibh tellus molestie nunc non blandit massa. </p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras.</p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Diam maecenas ultricies mi eget. Dis parturient montes nascetur ridiculus mus mauris. </p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Yap yap yap, yap yap yap yap yap. Yap yap yap.</p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Vel quam elementum pulvinar etiam non quam. Purus ut faucibus pulvinar elementum. </p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Pharetra pharetra massa massa ultricies mi quis hendrerit. At in tellus integer feugiat.</p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada. </p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Tortor aliquam nulla facilisi cras. Lorem ipsum dolor sit amet consectetur. </p>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>A condimentum vitae sapien pellentesque habitant morbi tristique senectus. </p>
+      <br /><br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <p>Posuere ac ut consequat semper. Ac orci phasellus egestas tellus rutrum. </p>
+    </div>
+  );
+}
