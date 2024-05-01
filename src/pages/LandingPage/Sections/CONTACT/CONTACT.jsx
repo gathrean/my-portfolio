28a424a5e214222e4c42ff63957c681ec4c247e1
Gathrean Dela Cruz <gathrean@icloud.com>
2024-05-01 10:54:23 -0700

    Added new .jsx files for the Landing Page sectiosn (HOME, WORK, TECHSTACK, CONTACT)

---
+++ src/pages/LandingPage/Sections/CONTACT/CONTACT.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function CONTACT() {
+    return (
+        <section id="section2 about">
+            <div className="content">
+                <h2>Section: Contact</h2>
+                {/* Add about content here */}
+            </div>
+        </section>
+    );
+};
