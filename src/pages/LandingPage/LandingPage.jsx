cffcca4e8eeaec9de8efc772072aea3debac8b59
Gathrean Dela Cruz <gathrean@icloud.com>
2024-05-02 01:22:17 -0700

    Moved Hero section from LandingPage into its own class, HERO.jsx Also changed image

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+
+// Sections
+import HERO from './Sections/HERO/HERO';
 import { WORK } from './Sections/WORK/WORK';
 import { TECH } from './Sections/TECH/TECH';
 import { CONTACT } from './Sections/CONTACT/CONTACT';
@@ -12,70 +15,25 @@ import '../../App.css';
 import '../../assets/fonts/Fonts.css';
 
 export function LandingPage() {
-    const [currentImage, setCurrentImage] = useState(0);
     const images = [
-        '/images/DSC04733.jpg'
+        '/images/DSC04733v2.jpg'
     ];
-
-    // Ref for overlay text element
-    const overlayRef = useRef(null);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-        }, 5000); // Change image every 5 seconds (5000 milliseconds)
-
-        return () => clearInterval(intervalId);
-    }, []);
-
-    // Function to calculate overlay text height
-    const calculateOverlayHeight = () => {
-        if (overlayRef.current) {
-            return overlayRef.current.getBoundingClientRect().height;
-        }
-        return 0;
-    };
-
-    // Add state to manage the active section for highlighting navbar links
-    const [activeSection, setActiveSection] = useState('HOME');
-
-    // Function to handle scrolling to the clicked section
-    const scrollToSection = (section) => {
-        const element = document.getElementById(section);
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth' });
-            setActiveSection(section);
-        }
-    };
 
     return (
         <section id="section1 home">
-            <div className="carousel-content">
-                <div className="image-container">
-                    <img src={process.env.PUBLIC_URL + images[currentImage]} alt="Landing Page" className="landing-image" />
-                </div>
-                <div className="overlay-container" style={{ marginTop: calculateOverlayHeight() }}>
-                    {/* <div ref={overlayRef} className="overlay-text">
-                        <h2>Hey, my name is Gathrean Dela Cruz</h2>
-                        <p>I am a software developer from Vancouver, BC. <br />
-                           Let's connect on LinkedIn! (button) <br />
-                        Check out my GitHub! (button) <br />
-                        </p>
-                    </div> */}
-                    <section id="HOME" className="HOME">
-                        {/* Your HOME section content here */}
-                    </section>
-                    <section id="WORK" className="WORK">
-                        <WORK />
-                    </section>
-                    <section id="TECH" className="TECH">
-                        <TECH />
-                    </section>
-                    <section id="CONTACT" className="CONTACT">
-                        <CONTACT />
-                    </section>
-                </div>
-            </div>
+            <HERO images={images} />
+            <section id="HOME" className="HOME">
+                {/* Your HOME section content here */}
+            </section>
+            <section id="WORK" className="WORK">
+                <WORK />
+            </section>
+            <section id="TECH" className="TECH">
+                <TECH />
+            </section>
+            <section id="CONTACT" className="CONTACT">
+                <CONTACT />
+            </section>
         </section>
     );
-};
+}
---
+++ src/pages/LandingPage/Sections/HERO/HERO.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+import './HERO.css'
+
+const HERO = ({ images }) => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  // Ref for overlay text element
+  const overlayRef = useRef(null);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 5000); // Change image every 5 seconds (5000 milliseconds)
+
+    return () => clearInterval(intervalId);
+  }, [images]);
+
+  return (
+    <div className="carousel-content">
+
+      <div className="image-container">
+        <img src={process.env.PUBLIC_URL + images[currentImage]} alt="Landing Page" className="landing-image" />
+      </div>
+
+      <div className="overlay-container">
+
+        <div ref={overlayRef} className="overlay-text">
+          <h2>Hey, I am</h2>
+          <h1>Gathrean Dela Cruz</h1>
+          <h3>Software & Web Developer.</h3>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default HERO;
