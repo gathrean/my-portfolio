ffd74e58b0d452755f5f242676627c04ed3c9e5a
Gathrean Dela Cruz <gathrean@icloud.com>
2024-05-04 01:29:45 -0700

    Changed padding of projects container to 10vw

--- src/pages/LandingPage/Sections/PROJECTS/PROJECTS.jsx
+++ src/pages/LandingPage/Sections/PROJECTS/PROJECTS.jsx
@@ -4,7 +4,7 @@ import './PROJECTS.css';
 
 export function PROJECTS() {
   return (
-    <div className="work-section-container">
+    <div className="projects-section-container">
       <h1>PROJECTS</h1>
       <p>This is where you can showcase your work experience. You can list your previous jobs, projects you've worked on, and any other relevant experience.</p>
     </div>
