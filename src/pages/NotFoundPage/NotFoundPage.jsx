53ecf75fa540842afe710ad0967c51e7e3cc8861
Gathrean Dela Cruz <gathrean@icloud.com>
2024-05-03 18:09:04 -0700

    Added font and extra styling to Error

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './NotFoundPage.css';
 
 export function NotFoundPage() {
   return (
-    <div>
+    <div className="error-container">
       <h1>404 - Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <p>The page you are looking for does not exist, or still not yet implemented.</p>
+      <Link to="/" className="home-button">Go Home</Link>
     </div>
   );
 };
