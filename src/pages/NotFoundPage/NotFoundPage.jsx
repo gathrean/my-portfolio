78385bd9c4ba48de596d0e406e1dee30955bc3c7
Gathrean Dela Cruz <gathrean@icloud.com>
2024-05-04 02:10:13 -0700

    Added logic for closeTabIfHomePage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -4,12 +4,21 @@ import { Link } from 'react-router-dom';
 import './NotFoundPage.css';
 
 export function NotFoundPage() {
+
+  const closeTabIfHomePage = () => {
+    const currentPageUrl = window.location.href;
+    const homePageUrl = '/';
+    if (currentPageUrl !== homePageUrl) {
+      window.close();
+    }
+  };
+
   return (
     <div className="error-container">
       <h1>404</h1>
       <h1>Page Not Found</h1>
       <p className="error-message">The page you are looking for does not exist, or still not yet implemented.</p>
-      <Link to="/" className="home-button">Go Home</Link>
+      <Link to="/" className="home-button" onClick={closeTabIfHomePage}>Go Home</Link>
       <p className="ascii">
         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠖⠚⠉⠁⠀⠀⠉⠙⠒⢄⠀⠀⠀⠀⠀⠀
