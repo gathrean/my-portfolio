7c39c7148f3202cd16b506b69de98843b687d1b9
Gathrean Dela Cruz <gathrean@icloud.com>
2024-05-04 01:40:37 -0700

    Added hr in PROJECTS

--- src/pages/LandingPage/Sections/PROJECTS/PROJECTS.jsx
+++ src/pages/LandingPage/Sections/PROJECTS/PROJECTS.jsx
@@ -4,6 +4,7 @@ import './PROJECTS.css';
 export function PROJECTS() {
   return (
     <div className="projects-section-container">
+      <hr />
       <h1>Featured Projects</h1>
       <p>This is where you can showcase your work experience. You can list your previous jobs, projects you've worked on, and any other relevant experience.</p>
       <div className="projects-grid">
