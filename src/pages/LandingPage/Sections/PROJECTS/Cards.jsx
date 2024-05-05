7fdcb555d9f397ba49f027f44c24c4a8bf557473
Gathrean Dela Cruz <gathrean@icloud.com>
2024-05-05 16:09:41 -0700

    Added whiteText and blackText const variables

--- src/pages/LandingPage/Sections/PROJECTS/Cards.jsx
+++ src/pages/LandingPage/Sections/PROJECTS/Cards.jsx
@@ -7,32 +7,35 @@ function ProjectCard({ projectName, imageURL, keywords, date, description }) {
 
   const keywordArray = keywords.split(',').map(keyword => keyword.trim());
 
+  const whiteText = "#fff";
+  const blackText = "#000";
+
   const keywordColors = {
-    "HTML": { background: "#DA4E25", text: "#fff" },
-    "CSS": { background: "#244DDF", text: "#fff" },
-    "JavaScript": { background: "#E8D44D", text: "#000" },
-    "Python": { background: "#4B8BBE", text: "#fff" },
-    "TensorFlow": { background: "#FF6F00", text: "#fff" },
-    "PyTorch": { background: "#EE4C2C", text: "#fff" },
-    "EJS": { background: "#8A5DA0", text: "#fff" },
-    "Bootstrap": { background: "#7952B3", text: "#fff" },
-    "Node.js": { background: "#68A063", text: "#fff" },
-    "Express.js": { background: "#F2C621", text: "#000" },
-    "Leaflet API": { background: "#199900", text: "#fff" },
-    "MongoDB": { background: "#4DB33D", text: "#fff" },
-    "Firebase": { background: "#FFCA28", text: "#000" },
-    "Java": { background: "#896157", text: "#fff" },
-    "Kotlin": { background: "#C220D8", text: "#FFFFFF" },
-    "AndroidStudio": { background: "#A8C840", text: "#000" },
-    "PokeAPI": { background: "#F44336", text: "#fff" },
-    "Vite": { background: "#646CFF", text: "#fff" },
-    "Supabase": { background: "#3EC8AC", text: "#fff" },
-    "IntelliJ": { background: "#0777F1", text: "#fff" },
-    "Java Processing": { background: "#006699", text: "#fff" },
-    "Gradle": { background: "#02303A", text: "#fff" },
-    "ChatGPT API": { background: "#75A99B", text: "#fff" },
-    "React": { background: "#61DAFB", text: "#000" },
-    "default": { background: "#E2E2E2", text: "#000" }
+    "HTML": { background: "#DA4E25", text: whiteText },
+    "CSS": { background: "#244DDF", text: whiteText },
+    "JavaScript": { background: "#E8D44D", text: blackText },
+    "Python": { background: "#4B8BBE", text: whiteText },
+    "TensorFlow": { background: "#FF6F00", text: whiteText },
+    "PyTorch": { background: "#EE4C2C", text: whiteText },
+    "EJS": { background: "#8A5DA0", text: whiteText },
+    "Bootstrap": { background: "#7952B3", text: whiteText },
+    "Node.js": { background: "#68A063", text: whiteText },
+    "Express.js": { background: "#F2C621", text: blackText },
+    "Leaflet API": { background: "#199900", text: whiteText },
+    "MongoDB": { background: "#4DB33D", text: whiteText },
+    "Firebase": { background: "#FFCA28", text: blackText },
+    "Java": { background: "#896157", text: whiteText },
+    "Kotlin": { background: "#C220D8", text: whiteText },
+    "AndroidStudio": { background: "#A8C840", text: blackText },
+    "PokeAPI": { background: "#F44336", text: whiteText },
+    "Vite": { background: "#646CFF", text: whiteText },
+    "Supabase": { background: "#3EC8AC", text: whiteText },
+    "IntelliJ": { background: "#0777F1", text: whiteText },
+    "Java Processing": { background: "#006699", text: whiteText },
+    "Gradle": { background: "#02303A", text: whiteText },
+    "ChatGPT API": { background: "#75A99B", text: whiteText },
+    "React": { background: "#61DAFB", text: blackText },
+    "default": { background: "#E2E2E2", text: blackText }
   };
 
   return (
