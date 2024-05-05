45daf5d113efea44bb3f3e818296a278c93ae1ee
Gathrean Dela Cruz <gathrean@icloud.com>
2024-05-05 15:06:22 -0700

    Removed custom project card backgrounds

--- src/pages/LandingPage/Sections/PROJECTS/ProjectCard.jsx
+++ src/pages/LandingPage/Sections/PROJECTS/ProjectCard.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 
 import './PROJECTS.css';
 
-function ProjectCard({ projectName, imageURL, keywords, date, description, backgroundColor }) {
+function ProjectCard({ projectName, imageURL, keywords, date, description, }) {
   const keywordArray = keywords.split(',').map(keyword => keyword.trim());
 
-  const cardStyle = {
-    backgroundColor: backgroundColor || '#FFFFFF',
-    color: backgroundColor ? '#FFFFFF' : '#000000',
-  };
-
   return (
-    <div className="project-card" style={cardStyle}>
-      
+    <div className="project-card">
+
       <a href="#placeholder" className="header-image-link">
         <div className="header-image">
           <img src={imageURL} alt={projectName} />
@@ -26,8 +21,8 @@ function ProjectCard({ projectName, imageURL, keywords, date, description, backg
         </div>
       </a>
 
-      <h1 style={{ color: backgroundColor ? '#FFFFFF' : '#000000' }}>{projectName}</h1>
-      <h3 style={{ color: backgroundColor ? '#FFFFFF' : '#4b4b4b' }}>{date}</h3>
+      <h1>{projectName}</h1>
+      <h3>{date}</h3>
 
       <div className="keywords">
         {keywordArray.map((keyword, index) => (
@@ -35,7 +30,7 @@ function ProjectCard({ projectName, imageURL, keywords, date, description, backg
         ))}
       </div>
 
-      <p style={{ color: backgroundColor ? '#FFFFFF' : '#616161' }}>{description}</p>
+      <p >{description}</p>
       <a href="#" className="read-more-link">Read more</a>
     </div>
   );
