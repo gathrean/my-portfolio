643620572fbbe2f65c086013a86706dd9df5a13e
Gathrean Dela Cruz <gathrean@icloud.com>
2024-05-05 14:36:46 -0700

    Added default and customizable styles for backgroundColor, h1, h3, and p elements (ProjectCard)

--- src/pages/LandingPage/Sections/PROJECTS/ProjectCard.jsx
+++ src/pages/LandingPage/Sections/PROJECTS/ProjectCard.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 
 import './PROJECTS.css';
 
-function ProjectCard({ projectName, imageURL, keywords, date, description }) {
+function ProjectCard({ projectName, imageURL, keywords, date, description, backgroundColor }) {
   const keywordArray = keywords.split(',').map(keyword => keyword.trim());
 
+  const cardStyle = {
+    backgroundColor: backgroundColor || '#FFFFFF',
+    color: backgroundColor ? '#FFFFFF' : '#000000',
+  };
+
   return (
-    <div className="project-card">
+    <div className="project-card" style={cardStyle}>
+      
       <a href="#placeholder" className="header-image-link">
         <div className="header-image">
-        <img src={imageURL} alt={projectName} />
+          <img src={imageURL} alt={projectName} />
           <div className="wrapper">
             <div className="marquee">
               <p className="readme-text">
@@ -19,14 +25,17 @@ function ProjectCard({ projectName, imageURL, keywords, date, description }) {
           </div>
         </div>
       </a>
-      <h1>{projectName}</h1>
-      <h3>{date}</h3>
+
+      <h1 style={{ color: backgroundColor ? '#FFFFFF' : '#000000' }}>{projectName}</h1>
+      <h3 style={{ color: backgroundColor ? '#FFFFFF' : '#4b4b4b' }}>{date}</h3>
+
       <div className="keywords">
         {keywordArray.map((keyword, index) => (
           <span key={index} className="keyword">{keyword}</span>
         ))}
       </div>
-      <p>{description}</p>
+
+      <p style={{ color: backgroundColor ? '#FFFFFF' : '#616161' }}>{description}</p>
       <a href="#" className="read-more-link">Read more</a>
     </div>
   );
