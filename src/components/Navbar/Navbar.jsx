9487f17812308cf261e3b630b45e4680e5f1ac92
Gathrean Dela Cruz <gathrean@icloud.com>
2024-05-05 22:22:53 -0700

    Removed unused code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,10 +10,9 @@ import hamburgerImage from './Icons/hamburger-white.png';
 
 export function Navbar() {
     const { navbarHeight, navbarRef, toggleHamburger } = useNavbarFunctions();
-    
+
     const [activeSection, setActiveSection] = useState('HOME');
     const [collapsed] = useState(false);
-    const [showNavbar] = useState(false);
 
     const scrollToSection = (sectionClassName) => {
         if (activeSection !== sectionClassName) {
@@ -30,40 +29,41 @@ export function Navbar() {
     };
 
     return (
-        <nav
-            ref={navbarRef}
-            className={`navbar navbar-animation ${showNavbar ? 'slide-down' : ''}`}
-            style={{ height: navbarHeight }}
-        >
-            <div className="navbar-container navbar-animation">
+        <nav ref={navbarRef} className="navbar navbar-animation" style={{ height: navbarHeight }}>
+
+            <div className="navbar-container">
+
                 <div className="brand">
                     <span className="brand-text">Gathrean</span>
                 </div>
+
                 <div className="mobile-view" onClick={toggleHamburger}>
                     <img
                         src={hamburgerImage}
-                        alt="Hamburger Menu"
                         className={`hamburger ${collapsed ? 'collapsed' : 'open'}`}
                         style={{ width: '30px', height: '30px' }}
                     />
                 </div>
+
             </div>
+
             <div className="navlink-container">
                 <ul className={`navlink-ul ${collapsed ? 'collapsed' : 'closed'}`}>
                     <li>
                         <Link>About Me</Link>
                     </li>
                     <li>
-                        <Link className={`${activeSection === 'PROJECTS' ? 'highlight' : ''}`} onClick={() => scrollToSection('PROJECTS')}>Projects</Link>
+                        <Link onClick={() => scrollToSection('PROJECTS')}>Projects</Link>
                     </li>
                     <li>
                         <Link>Work</Link>
                     </li>
                     <li>
-                        <Link className={`${activeSection === 'CONTACT' ? 'highlight' : ''}`} onClick={() => scrollToSection('CONTACT')}>Contact Me</Link>
+                        <Link onClick={() => scrollToSection('CONTACT')}>Contact Me</Link>
                     </li>
                 </ul>
             </div>
+
         </nav>
     );
 }
