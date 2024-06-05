6da962012559941d4e2b9cca8ec6dc7d4f3fbce6
Gathrean Dela Cruz <gathrean@icloud.com>
2024-06-04 19:41:00 -0700

    Added github contributions and my local time

---
+++ src/components/LocalTime.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+function LocalTime() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    const options = {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    };
+    const localTime = time.toLocaleTimeString([], options);
+
+    return <p>{localTime}</p>;
+}
+
+export default LocalTime;
