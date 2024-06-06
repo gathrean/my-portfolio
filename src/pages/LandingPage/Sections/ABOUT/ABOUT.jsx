afd357f94b6f62b00fa78e5e4ea7081bd3e25508
Gathrean Dela Cruz <gathrean@icloud.com>
2024-06-06 14:44:57 -0700

    Separated MyProfile into its own component react file

--- src/pages/LandingPage/Sections/ABOUT/ABOUT.jsx
+++ src/pages/LandingPage/Sections/ABOUT/ABOUT.jsx
@@ -1,33 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 // Styles
 import './ABOUT.css';
 
 // Components
-import SocialMediaIcons from '../../../../components/SocialMediaIcons/SocialMediaIcons';
-import LocalTime from '../../../../components/LocalTime';
-import GathreanIcon from '../../../../assets/icons/project-icons/gathrean-Icon.png';
+import MyProfile from '../../MyProfile/MyProfile';
 import ArrowRightIcon from '../../../../components/ArrowRight/ArrowRightBlack';
 
 // Data
 import aboutTexts from './aboutTexts.json';
 
 export function ABOUT() {
-  const [emailMessage, setEmailMessage] = useState('📧 gathrean@icloud.com');
-
-  const handleEmailClick = () => {
-    navigator.clipboard.writeText('gathrean@icloud.com')
-      .then(() => {
-        setEmailMessage('💡 Email copied to clipboard!');
-        setTimeout(() => {
-          setEmailMessage('📧 gathrean@icloud.com');
-        }, 2000);
-      })
-      .catch(err => {
-        console.error('😵 Failed to copy email: ', err);
-      });
-  };
-
   return (
     <div className="section-container">
       <hr />
@@ -35,25 +18,7 @@ export function ABOUT() {
       <p>Here's a bit about me: a passionate software developer committed to continuous learning and innovation.</p>
       <div className="section-container-child">
         <div className="about-me-container">
-
-          <div className="my-profile">
-            <div className="about-me-images">
-              <img src={GathreanIcon} alt="Gathrean" />
-            </div>
-            <br />
-            <div className="about-me-info">
-              <h3>Gathrean Dela Cruz</h3>
-              <p><b>Software Developer</b></p>
-              <p>🎂 23 years old</p>
-              <p>🇨🇦 Vancouver, BC, Canada</p>
-              <p>⌚️ <LocalTime /></p>
-              <p onClick={handleEmailClick} style={{ cursor: 'pointer' }}>{emailMessage}</p>
-              <div className="social-media-icons">
-                <SocialMediaIcons />
-              </div>
-            </div>
-          </div>
-
+          <MyProfile />
           <div>
             <h4>Personal Interests and Values</h4>
             <div className="about-cards">
