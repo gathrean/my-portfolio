d5aef15ebacca35c01042b59a08c6cb9b39f2d6d
Gathrean Dela Cruz <gathrean@icloud.com>
2024-06-22 15:49:48 -0700

    Added an infinite scroll for skill keywords

--- src/pages/LandingPage/Sections/ABOUT/ABOUT.jsx
+++ src/pages/LandingPage/Sections/ABOUT/ABOUT.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 // Styles
 import './ABOUT.css';
+import './InfiniteScroll.css';
 
 export function ABOUT() {
   return (
@@ -67,6 +68,34 @@ export function ABOUT() {
           </div>
 
           <br />
+
+          
+          <div className="scroll" style={{ '--time': '30s' }}>
+            <div>
+              <span>HTML</span>
+              <span>CSS</span>
+              <span>JavaScript</span>
+              <span>ReactJS</span>
+              <span>Figma</span>
+              <span>Photoshop</span>
+              <span>Premiere Pro</span>
+              <span>Figma</span>
+              <span>Angular</span>
+              <span>Node JS</span>
+            </div>
+            <div>
+              <span>HTML</span>
+              <span>CSS</span>
+              <span>JavaScript</span>
+              <span>ReactJS</span>
+              <span>Figma</span>
+              <span>Photoshop</span>
+              <span>Premiere Pro</span>
+              <span>Figma</span>
+              <span>Angular</span>
+              <span>Node JS</span>
+            </div>
+          </div>
 
         </div>
       </div>
