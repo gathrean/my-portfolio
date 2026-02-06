d95a891326c377f0a28338d70fe6f6f798238dbd
Gathrean Dela Cruz <gathrean@icloud.com>
2026-02-06 02:32:34 -0800

    Removed emojis

--- src/pages/LandingPage/Sections/ABOUT/ABOUT.jsx
+++ src/pages/LandingPage/Sections/ABOUT/ABOUT.jsx
@@ -16,13 +16,11 @@ export function ABOUT() {
             <p>
               I remember being a kid in 2008 and always wondered how websites and games were created. Fast forward to 2024, I've completed "Computer Systems Technology", a 2-year program at BCIT, and found my passion in software development.
             </p>
-            <h4 className="emojis">🎓  💻 🕹️</h4>
           </div>
           <div className="card">
             <p>
               Beyond coding, I enjoy music production and graphic design- which are hobbies that have helped my creativity and innovation grow in software development. I also enjoy spending time with family and friends, and exploring nature and travelling new cities.
             </p>
-            <h4 className="emojis">🎧  🎨  🌁</h4>
           </div>
         </div>
       </div>
